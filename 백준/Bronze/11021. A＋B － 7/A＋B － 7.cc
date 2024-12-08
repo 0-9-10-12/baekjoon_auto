0ab051af13c1608b1d469e940f69d5b079b26324
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2024-12-08 22:06:55 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int input{};
+	std::cin >> input;
+	
+	for (int i{}; i < input; ++i) {
+		int a{}, b{};
+		std::cin >> a >> b;
+		std::cout << "Case #" << i + 1 << ": "<< a + b << '\n';
+	}
+}
