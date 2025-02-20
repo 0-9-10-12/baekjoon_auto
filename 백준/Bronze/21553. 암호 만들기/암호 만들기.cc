f2261674ca444ca1822df4367ef9034ad98cdd27
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-02-20 10:43:25 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	std::string A{};
+	std::string P{};
+
+	std::cin >> A >> P;
+	std::cout << P;
+}
