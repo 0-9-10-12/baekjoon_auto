bf35f783e66d97d73eaeb4e76f73e60962f673db
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-05-02 14:53:40 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	int n, m;
+
+	while(std::cin >> n >> m) {
+		if (n == 0 && m == 0) {
+			break;
+		}
+
+		std::cout << n + m << "\n";
+	}
+	
+}
