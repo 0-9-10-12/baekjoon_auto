7d273e417fd35e78d3edd99f2da62b3c08ebe9ef
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-05-04 18:32:40 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	int sum{};
+	for (int i{}; i < 5; ++i)
+	{
+		int a;
+		std::cin >> a;
+		sum += a;
+	}
+
+	std::cout << sum << "\n";
+}
+
