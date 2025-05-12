151ce728f1a7d4503c9fc563e4f18b366da35d6c
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-05-12 16:10:11 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	int n;
+	std::cin >> n;
+
+	if (n == 0) {
+		std::cout << "YONSEI";
+	}
+	if (n == 1) {
+		std::cout << "Leading the Way to the Future";
+	}
+}
