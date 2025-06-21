0bcd48e08db6acaeca879d3e4b7b6fdd9344d43a
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-06-21 22:57:19 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int a, b;
+	std::cin >> a >> b;
+    
+    std::cout << 1000 * a + 10000 * b;
+}
