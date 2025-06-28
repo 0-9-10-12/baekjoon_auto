7e9611b320328d8a2b30d046719c2b4a869f30a2
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-06-28 20:06:35 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    std::string a, b;
+    std::cin >> a >> b;
+
+    if (a.size() < b.size()) {
+        std::cout << "no";
+    }
+    else {
+        std::cout << "go";
+    }
+
+}
