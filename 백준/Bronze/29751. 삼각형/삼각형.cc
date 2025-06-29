3168b9b81f54e380b390712b7f46ecf3e78c969f
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-06-29 14:19:11 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	double a, b;
+	std::cin >> a >> b;
+
+	double c{ a * b * 0.5 };
+	
+	std::cout << std::fixed;
+	std::cout.precision(1);
+	
+	std::cout << c;
+}
