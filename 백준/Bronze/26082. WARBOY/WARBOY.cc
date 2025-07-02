de33caf926aa0303b54bc8cbcaf0f71b256ef1c3
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-07-03 02:00:06 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	int d{ b / a };
+	int ans{ 3 * d * c };
+
+	std::cout << ans;
+}
