d969349067974d81456aa30d9524e80d7c7942d0
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-07-13 01:09:05 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int a;
+    std::cin >> a;
+
+    for (int i{}; i < a; ++i) {
+        int b;
+        std::cin >> b;
+		std::cout << b << " " << b << "\n";
+    }
+}
