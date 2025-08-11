45fc8f5ec248071d7e605163bbb4ce67124b8830
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-11 15:04:18 +0900

    [Bronze IV] Title: Final Price, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28224. Final Price/Final Price.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+    std::cin >> n;
+
+    int ans{};
+    for (int i{}; i < n; ++i) {
+        int a;
+        std::cin >> a;
+        ans += a;
+    }
+
+    std::cout << ans;
+}
