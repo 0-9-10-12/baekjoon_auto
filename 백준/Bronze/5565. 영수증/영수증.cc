e5d6b55d02ca931072d7ec5ab61995bf061a3fe8
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-12 18:40:30 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int total;
+	std::cin >> total;
+
+    for (int i{}; i < 9; ++i) {
+        int a;
+        std::cin >> a;
+
+        total -= a;
+    }
+
+    std::cout << total;
+}
