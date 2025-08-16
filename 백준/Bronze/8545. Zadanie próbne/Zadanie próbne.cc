79c850bb279ab97a9bfe5153d5cdd7e1d4f18ef0
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-16 10:04:46 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    std::string s;
+    std::cin >> s;
+
+    for (int i{ (int)s.size() - 1 }; i >= 0; --i) {
+		std::cout << s[i];
+    }
+}
