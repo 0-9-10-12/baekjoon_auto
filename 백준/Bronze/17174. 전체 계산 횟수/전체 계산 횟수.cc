915f753057a256de2b892fcf20dfd4b1b9871ce4
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-17 22:44:10 +0900

    [Bronze III] Title: 전체 계산 횟수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17174. 전체 계산 횟수/전체 계산 횟수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int a, b;
+    std::cin >> a >> b;
+
+    int ans{ a };
+    
+    while (a >= b) {
+        a /= b;
+		ans += a;
+    }
+
+    std::cout << ans;
+}
