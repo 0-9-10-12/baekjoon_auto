0c84c4804c96b723d8b56e470d42c9fa6c9817c2
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-18 15:37:48 +0900

    [Bronze IV] Title: Identifying tea, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int t;
+	std::cin >> t;
+
+    int ans{};
+    for (int i{}; i < 5; ++i) {
+        int a;
+		std::cin >> a;
+
+        if (a == t) {
+            ++ans;
+        }
+    }
+
+    std::cout << ans;
+}
