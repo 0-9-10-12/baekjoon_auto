1b94c50a5138f4798cec110b5a6eb407620c3ab4
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-25 11:26:37 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+	std::cin >> n;
+
+    int ans[45]{0, 1, 1};
+    for (int i{3}; i < n + 1; ++i) {
+		ans[i] = ans[i - 1] + ans[i - 2];
+    }
+
+	std::cout << ans[n];
+}
