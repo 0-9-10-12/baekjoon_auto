f8fbb0c0fa752b4658b666dcd1b899d92ce6ce52
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-26 23:37:16 +0900

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int m, k;
+	std::cin >> m >> k;
+
+    if (m % k == 0) {
+        std::cout << "Yes";
+	}
+    else {
+		std::cout << "No";
+    }
+}
