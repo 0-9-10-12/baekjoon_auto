eb2c42665015c49676e1a68c00dbc76386c635d0
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-08-30 18:45:46 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int sum{};
+    for (int i{}; i < 4; ++i) {
+        int a;
+		std::cin >> a;
+		sum += a;
+    }
+
+	std::cout << sum / 60 << '\n' << sum % 60;
+}
