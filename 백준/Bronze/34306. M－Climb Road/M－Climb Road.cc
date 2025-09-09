7dc6239f644dbf069ec038e6f830d276a5e58d63
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-09-09 19:02:28 +0900

    [Bronze IV] Title: M-Climb Road, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34306. M－Climb Road/M－Climb Road.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	int w, n;
+	std::cin >> w >> n;
+
+	std::cout << w * 5'280 / n;
+}
