2aa4e8ff2ae745b619133c693a6a68204ca2bb91
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-09-10 13:31:56 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    while (true) {
+        //std::cin.ignore();
+        std::string s;
+        std::getline(std::cin, s);
+
+        if (s == "#") {
+            break;
+        }
+
+        int n{};
+        for (int i{}; i < s.size(); ++i) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'
+                || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                ++n;
+			}
+        }
+
+		std::cout << n << '\n';
+    }
+}
