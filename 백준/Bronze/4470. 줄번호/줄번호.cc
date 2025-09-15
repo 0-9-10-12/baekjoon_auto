948aeca2b29343a888141e540f3f5f65b02177ec
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-09-15 19:03:54 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+	std::cin >> n;
+	std::cin.ignore();
+
+    for (int i{}; i < n; ++i) {
+        std::string s;
+        std::getline(std::cin, s);
+
+		std::cout << i + 1 << ". " << s << "\n";
+    }
+}
