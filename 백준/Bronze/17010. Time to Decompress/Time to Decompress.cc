55ad12cbc3e8c5222c329ff1be2d4910909853ae
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-09-23 21:20:30 +0900

    [Bronze IV] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	int n;
+	std::cin >> n;
+	
+	for (int i{}; i < n; ++i) {
+		int a;
+		char c;
+		std::cin >> a >> c;
+
+		for (int j{}; j < a; ++j) {
+			std::cout << c;
+		}
+
+		std::cout << "\n";
+	}
+}
