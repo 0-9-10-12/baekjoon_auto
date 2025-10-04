ba897bb6ed5ab3b1b239da86113f6bd048f19141
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-04 21:14:40 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int a, b, c;
+    std::cin >> a >> b >> c;
+
+    if (a + b + c <= 21) {
+        std::cout << 1;
+    }
+    else {
+        std::cout << 0;
+	}
+}
