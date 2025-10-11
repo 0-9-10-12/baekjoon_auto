a0f40bea3d250b1680fed9a8408accd4de89d9b8
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-12 00:41:20 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+    std::cin >> n;
+
+    for (int i{}; i < n; ++i) {
+        int a, b;
+        std::cin >> a >> b;
+		std::cout << a + b << "\n";
+    }
+}
