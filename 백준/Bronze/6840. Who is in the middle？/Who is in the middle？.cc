d4d329f2dc50f148bb0c815142a0cd52db4fb436
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-12 17:39:56 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+	std::set<int> s;
+
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	s.insert(a);
+	s.insert(b);
+	s.insert(c);
+
+	std::cout << *(++s.begin());
+}
