217ae1db54efec44c54a4b6a0f24990c1ffb54ee
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-20 18:01:22 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int a;
+    std::cin >> a;
+
+    int ans{};
+    for (int i{}; i <= a; ++i) {
+        ans += (i * (i + 1));
+        
+        for (int j{}; j <= i; ++j) {
+            ans += j;
+        }
+    }
+
+	std::cout << ans;
+}
