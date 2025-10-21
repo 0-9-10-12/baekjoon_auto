06fe78fe22b98657134e72e96a807d0672b8d74a
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-21 15:42:13 +0900

    [Bronze III] Title: Изгороди, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    double n, a;
+	std::cin >> n >> a;
+
+    std::cout << static_cast<int>(std::round((180 - a) / 2 + a / 2));
+}
