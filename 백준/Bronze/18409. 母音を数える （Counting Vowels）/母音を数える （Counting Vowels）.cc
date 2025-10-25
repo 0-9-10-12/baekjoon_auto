f12496fd70ad08cbd64a30549172b590456a7df9
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-25 13:30:28 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+   FastIO;
+   Solution();
+}
+
+void Solution()
+{
+    int n;
+    std::string s;
+   std::cin >> n >> s;
+
+    int ans{};
+    for (int i{}; i < n; ++i) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+         ++ans;
+      }
+    }
+
+   std::cout << ans;
+}
