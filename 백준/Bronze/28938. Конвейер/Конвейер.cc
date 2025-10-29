005d858403feff6e304daf19e0d5da6db8af1b21
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-29 15:44:36 +0900

    [Bronze V] Title: Конвейер, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28938. Конвейер/Конвейер.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+    std::cin >> n;
+
+    int s{};
+    for (int i{}; i < n; ++i) {
+        int a;
+        std::cin >> a;
+		s += a;
+    }
+
+    if (s == 0) {
+        std::cout << "Stay";
+    }
+    else if (s > 0) {
+        std::cout << "Right";
+    }
+    else {
+        std::cout << "Left";
+	}
+}
