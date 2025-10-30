1cbf3cc0de40bdadbc86c271317c11f98ee420b8
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-30 11:53:40 +0900

    [Bronze V] Title: 13 Floors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+    std::cin >> n;
+
+    if (n < 13) {
+        std::cout << n;
+    }
+    else {
+        std::cout << n + 1;
+    }
+}
