4c1338203af9e2ae9d38e2c466a63cd2fa651f26
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-10-31 12:28:12 +0900

    [Bronze IV] Title: 특식 배부, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int n;
+    std::cin >> n;
+
+    int a, b, c;
+	std::cin >> a >> b >> c;
+
+    int ans{};
+    if (a > n) {
+        ans += n;
+    }
+    else {
+        ans += a;
+    }
+    if (b > n) {
+        ans += n;
+    }
+    else {
+        ans += b;
+    }
+    if (c > n) {
+        ans += n;
+    }
+	else {
+        ans += c;
+    }
+
+	std::cout << ans;
+}
