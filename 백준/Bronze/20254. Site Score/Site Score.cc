e32571b717be838ee6dcde303bca87ac0424c231
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-11-01 12:41:17 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <tuple>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <deque>
+
+#define FastIO                      \
+    std::ios::sync_with_stdio(false);    \
+    std::cin.tie(NULL);                  \
+    std::cout.tie(NULL);
+
+void Solution();
+
+int main() {
+	FastIO;
+	Solution();
+}
+
+void Solution()
+{
+    int ur, tr, uo, to;
+	std::cin >> ur >> tr >> uo >> to;
+
+	std::cout << 56 * ur + 24 * tr + 14 * uo + 6 * to;
+}
